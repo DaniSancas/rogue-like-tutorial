7b8b100086af6a0a406f5f142b207e49128d861f
DaniSancas <lord.sancas@gmail.com>
2022-11-20 14:33:58 +0100

    chore: refactor map to its own module

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use std::cmp::{max, min};
 mod component;
 pub use component::*;
 
-const MAX_WIDTH: i32 = 80;
-const MAX_HEIGHT: i32 = 50;
+mod map;
+pub use map::*;
 
 struct State {
     ecs: World,
@@ -37,7 +37,6 @@ impl State {
     }
 }
 
-
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
@@ -63,77 +62,6 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
             _ => (),
         },
-    }
-}
-
-#[derive(PartialEq, Clone, Copy)]
-enum TileType {
-    Wall,
-    Floor,
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * MAX_WIDTH as usize) + x as usize
-}
-
-fn new_map() -> Vec<TileType> {
-    let mut map = vec![TileType::Floor; MAX_WIDTH as usize * MAX_HEIGHT as usize];
-
-    // Make the boundaries walls
-    for x in 0..MAX_WIDTH {
-        map[xy_idx(x, 0)] = TileType::Wall;
-        map[xy_idx(x, MAX_HEIGHT - 1)] = TileType::Wall;
-    }
-
-    for y in 0..MAX_HEIGHT {
-        map[xy_idx(0, y)] = TileType::Wall;
-        map[xy_idx(MAX_WIDTH - 1, y)] = TileType::Wall;
-    }
-
-    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
-    // First, obtain the thread-local RNG:
-    let mut rng = rltk::RandomNumberGenerator::new();
-
-    for _i in 0..(MAX_WIDTH * MAX_HEIGHT / 10) {
-        let x = rng.roll_dice(1, MAX_WIDTH - 1);
-        let y = rng.roll_dice(1, MAX_HEIGHT - 1);
-        let idx = xy_idx(x, y);
-        if idx != xy_idx(MAX_WIDTH / 2, MAX_HEIGHT / 2) {
-            map[idx] = TileType::Wall;
-        }
-    }
-
-    map
-}
-
-fn draw_map(map: &[TileType], ctx: &mut Rltk) {
-    let mut x = 0;
-    let mut y = 0;
-
-    for tile in map.iter() {
-        match tile {
-            TileType::Floor => ctx.set(
-                x,
-                y,
-                RGB::from_f32(0.5, 0.5, 0.5),
-                RGB::from_f32(0., 0., 0.),
-                rltk::to_cp437('.'),
-            ),
-            TileType::Wall => ctx.set(
-                x,
-                y,
-                RGB::from_f32(0.0, 1.0, 0.0),
-                RGB::from_f32(0., 0., 0.),
-                rltk::to_cp437('#'),
-            ),
-        }
-
-        // Move the coordinates
-        x += 1;
-        if x >= MAX_WIDTH {
-            x = 0;
-            y += 1;
-        }
     }
 }
 
