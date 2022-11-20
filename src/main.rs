8d186de18e5a0f85b8a67876b38b86501c2dd101
DaniSancas <lord.sancas@gmail.com>
2022-11-20 13:40:12 +0100

    Draw the map with randomly generated walls

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use specs::prelude::*;
 use specs_derive::Component;
 use std::cmp::{max, min};
 
+const MAX_WIDTH: i32 = 80;
+const MAX_HEIGHT: i32 = 50;
+
 struct State {
     ecs: World,
 }
@@ -13,6 +16,9 @@ impl GameState for State {
 
         self.run_systems();
         player_input(self, ctx);
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -50,8 +56,8 @@ fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut players = ecs.write_storage::<Player>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        pos.x = min(MAX_WIDTH - 1, max(0, pos.x + delta_x));
+        pos.y = min(MAX_HEIGHT - 1, max(0, pos.y + delta_y));
     }
 }
 
@@ -68,6 +74,77 @@ fn player_input(gs: &mut State, ctx: &mut Rltk) {
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * MAX_WIDTH as usize) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; MAX_WIDTH as usize * MAX_HEIGHT as usize];
+
+    // Make the boundaries walls
+    for x in 0..MAX_WIDTH {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, MAX_HEIGHT - 1)] = TileType::Wall;
+    }
+
+    for y in 0..MAX_HEIGHT {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(MAX_WIDTH - 1, y)] = TileType::Wall;
+    }
+
+    // Now we'll randomly splat a bunch of walls. It won't be pretty, but it's a decent illustration.
+    // First, obtain the thread-local RNG:
+    let mut rng = rltk::RandomNumberGenerator::new();
+
+    for _i in 0..(MAX_WIDTH * MAX_HEIGHT) {
+        let x = rng.roll_dice(1, MAX_WIDTH - 1);
+        let y = rng.roll_dice(1, MAX_HEIGHT - 1);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(MAX_WIDTH / 2, MAX_HEIGHT / 2) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut Rltk) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.5, 0.5, 0.5),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('.'),
+            ),
+            TileType::Wall => ctx.set(
+                x,
+                y,
+                RGB::from_f32(0.0, 1.0, 0.0),
+                RGB::from_f32(0., 0., 0.),
+                rltk::to_cp437('#'),
+            ),
+        }
+
+        // Move the coordinates
+        x += 1;
+        if x >= MAX_WIDTH {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 fn main() -> rltk::RltkError {
     use rltk::RltkBuilder;
 
@@ -77,13 +154,18 @@ fn main() -> rltk::RltkError {
 
     let mut gs = State { ecs: World::new() };
 
+    gs.ecs.insert(new_map());
+
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: MAX_WIDTH / 2,
+            y: MAX_HEIGHT / 2,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
