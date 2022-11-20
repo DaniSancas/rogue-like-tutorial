01ab18b6b580286ee6b02a64e10ad135efb5b195
DaniSancas <lord.sancas@gmail.com>
2022-11-20 15:34:19 +0100

    Add parametrized tests to rect module

--- src/rect.rs
+++ src/rect.rs
@@ -7,7 +7,12 @@ pub struct Rect {
 
 impl Rect {
     pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
-        Self { x1: x, y1: y, x2: x + w, y2: y + h }
+        Self {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
     }
 
     pub fn intersect(&self, other: &Rect) -> bool {
@@ -16,5 +21,30 @@ impl Rect {
 
     pub fn center(&self) -> (i32, i32) {
         ((&self.x1 + &self.x2) / 2, (&self.y1 + self.y2) / 2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Rect;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(4, 6, true)]
+    #[case(5, 6, false)]
+    #[case(4, 7, false)]
+    fn test_does_intersect(#[case] x: i32, #[case] y: i32, #[case] expected: bool) {
+        let rect1 = Rect::new(1, 1, 3, 5);
+        let rect2 = Rect::new(x, y, y, 1);
+        assert_eq!(rect1.intersect(&rect2), expected);
+    }
+
+    #[rstest]
+    #[case((1, 1, 3, 5), (2, 3))]
+    #[case((1, 1, 4, 6), (3, 4))]
+    fn test_center(#[case] input: (i32, i32, i32, i32), #[case] expected: (i32, i32)) {
+        let (x, y, w, h) = input;
+        let rect = Rect::new(x, y, w, h);
+        assert_eq!(rect.center(), expected);
     }
 }
